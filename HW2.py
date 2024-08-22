24723ca4b734f03d862f8f06199fe308e1e77bc2
Eric Lin <el128221@Student.wpsok.org>
2024-08-22 12:52:10 -0500

    Added HW2

---
+++ HW2.py
@@ -0,0 +1,15 @@
+#Name: Eric Lin
+#class: 5th Hour
+#Assignment: HW2
+
+print('Hello World')
+
+
+print('What is your name')
+input("")
+print('Put one integers')
+x = input("")
+print("Give me another integer")
+y = input("")
+sum = int(y) + int(x)
+print(sum)
