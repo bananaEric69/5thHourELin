c11b46cd306046bf4f510f41bb4a81a95ad64be8
Eric Lin <el128221@Student.wpsok.org>
2024-08-29 12:42:46 -0500

    Added HW3

---
+++ HW3.py
@@ -0,0 +1,11 @@
+print('Hello world')
+
+santasList = ["Timmy", "Jimmy", "Kimmy", "Billy", "Bob"]
+santasList.append(input("please give name"))
+print(santasList[3])
+numList = [1, 2 ,3 ,4 ]
+numList.insert(1,6)
+print(numList)
+numList.sort()
+sum = numList[0] + numList[1] + numList[2]
+print(sum)
