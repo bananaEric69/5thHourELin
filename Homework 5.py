e38c672885d0227d897f43ce311b217a9f54a74e
Eric Lin <el128221@Student.wpsok.org>
2024-09-27 13:07:55 -0500

    added HW5

---
+++ Homework 5.py
@@ -0,0 +1,18 @@
+#Name: Eric Lin
+#Class: 5th Hour
+#Assignment: HW5
+
+print('Hello world')
+
+a = 10,
+b = 8,
+c = 4,
+
+
+
+if c > b and c > a:
+    print('C is the biggest number')
+elif b > a and b > c:
+    print("b is the biggest number")
+elif a > b and a > c:
+    print('a is the biggest number')
