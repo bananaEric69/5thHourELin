a431c491ba5cdfdae5c87c386bc656cd9dc9afc7
Eric Lin <el128221@Student.wpsok.org>
2024-10-03 12:59:02 -0500

    added HW6

--- HW6.py
+++ HW6.py
@@ -8,14 +8,21 @@ print('Hello world')
 num1 = int(input())
 
 if num1 % 2 == 0:
-    print('your number us divisible by 2')
-    x = num1 / 2
-    print(x)
-else num1 % 3 == 0:
-    print('your number is divisible by 2 ')
-    y = num1 / 3
-    print(y)
+   if num1 % 3 == 0:
+        print('your number is divisible by 2 and 3')
+        print(num1 / 2)
+        print(num1 / 3)
+   else:
+       print("The number is not divisible by 3")
+       print(num1 / 2)
 else:
+    if num1 % 3 == 0:
+        print('your number is not divisible by 2 ')
+        print(num1 / 3)
+    else:
+        print("Your number is not divisible by 2 or 3")
+
+
 
 
 
