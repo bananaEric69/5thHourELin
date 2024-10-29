5efcbcdc7b7722709ffcdd6d0ca276c849313d64
Eric Lin <el128221@Student.wpsok.org>
2024-10-29 13:18:08 -0500

    Added Hw10

---
+++ HW10.py
@@ -0,0 +1,34 @@
+#Name:Eric Lin
+#Class: 5th Hour
+#Assignment: HW10
+import time
+
+#1. Create a while loop with variable i that counts down from 5 to 0
+#and then prints "Hello World!" at the end.
+
+#i = 5
+#while i >= 0:
+ #    print(i)
+  #   time.sleep(1)
+  #   i -= 1
+#else:
+#    print("Hello World")
+
+
+
+#2. Create a while loop that prints only even numbers
+#between 1 and 30.
+#e = 0
+#while e <= 30:
+#    print(e)
+#    time.sleep(1)
+#    e += 2
+
+#3. Create a while loop that repeats until the user
+#inputs the number 0.
+o = 0
+
+while o <= 10000:
+    print(o)
+    time.sleep(0.5)
+    o += 1
