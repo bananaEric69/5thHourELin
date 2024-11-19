34dd5c0375bcb4ae3bc2617d54354802163728cc
Eric Lin <el128221@Student.wpsok.org>
2024-11-19 13:17:54 -0600

    Added Scenario 4

--- Scenario 4.py
+++ Scenario 4.py
@@ -1,7 +1,7 @@
 #Name:Eric Lin
 #Class: 5th Hour
 #Assignment: Scenario 4
-
+from lib2to3.fixes.fix_input import context
 
 #Scenario 4:
 #Due to scope creep leading to high development costs, the RPG you were working on has been
@@ -11,7 +11,18 @@
 #They want to start prototyping the rating system and are asking you to make it.
 #This prototype needs to allow the user to input the number of players, let each player rate
 #a single model from 1 to 5, and then give the average score of all of the ratings.
+x = 0
 
 print("Hello world")
+while 5 < 3:
+
 player = int(input())
-x =
+print(player)
+print("add how many player you need that is more than 0")
+if player <= 12:
+    print("more than 0 please")
+
+player = int(input())
+print(player)
+if player < 5:
+    print("Give a rating more than 0")
