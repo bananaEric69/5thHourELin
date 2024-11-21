c710cd24444d0e092408371fca6058b6415a5ecc
Eric Lin <el128221@Student.wpsok.org>
2024-11-21 13:19:28 -0600

    Added Scenario 4

--- Scenario 4.py
+++ Scenario 4.py
@@ -11,16 +11,15 @@
 #They want to start prototyping the rating system and are asking you to make it.
 #This prototype needs to allow the user to input the number of players, let each player rate
 #a single model from 1 to 5, and then give the average score of all of the ratings.
-e = 0
-print("Hello world")
-while 5 < 3:
-    print("add how many players needed")
-    player = int(input("**"))
-    print(player)
-    if player < 1:
-        print("")
-
-continue
-
-
-
+player = int(input("Enter a amount of players"))
+i = 1
+rating_sum = 0
+while i <= player:
+    rate = int(input("Rate 1-5"))
+    if rate > 5 or rate < 1:
+        print("error")
+    else:
+        rating_sum += rate
+        i += 1
+average = rating_sum / player
+print(average)
