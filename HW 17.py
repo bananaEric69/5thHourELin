746e153b02128bca1ae43ec8f934ba2e45dcc92a
Eric Lin <el128221@Student.wpsok.org>
2025-01-23 12:44:41 -0600

    Added HW 17

--- HW 17.py
+++ HW 17.py
@@ -11,24 +11,26 @@ def hello():
 #2. Create two empty integer variables named "heads" and "tails"
 heads = 0
 tails = 0
-
+coin = 0
 #3. Create a def function that flips a coin one hundred times and increments the result in the above variables.
 def coin_flip():
     global heads
     global tails
+    global coin
+    global flip
+    while not coin == 100:
+        coin += 1
+        flip = random.randint(1, 2)
+        if flip == 1:
+            heads += 1
+        elif flip == 2:
+            tails += 1
 
-while not 100:
-    coin = random.randint(1, 101)
-    print(coin)
-    heads += 1
-    tails += 1
 
-
-
-
-
-coin_flip()
 
 #4. Call the "Hello world" and "Coin Flip" functions here
-
+hello()
+coin_flip()
 #5. Print the final result of heads and tails here
+print(heads)
+print(tails)
