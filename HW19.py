451443a0d0e2de3ee6f5e270c80eb31d6e0912d5
Eric Lin <el128221@Student.wpsok.org>
2025-01-31 12:49:25 -0600

    Added HW 19

---
+++ HW19.py
@@ -0,0 +1,14 @@
+#Name:
+#Class: 5th Hour
+#Assignment: HW19
+import HW15
+#1. Import all the functions from the HW15 file.
+def hw15():
+    hello_world()
+    calculate(24, 12, 4)
+    animal("Zebra","Lion","Flamingo","Monkey","Hippo")
+    number(x = int(input("Please enter number")))
+
+#2. Call the functions here and run HW19.
+hw15()
+#3. Delete all calls from HW15 and run HW19 again.
