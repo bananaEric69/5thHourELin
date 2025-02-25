2d33c3d83e8b177ec4fb40b9a8b95320c377beaa
Eric Lin <el128221@Student.wpsok.org>
2025-02-25 13:18:07 -0600

    changes to playground

--- playground.py
+++ playground.py
@@ -3,21 +3,51 @@
 #Assigment: Playground
 
 import random
+#The icon signs of the slot machine
+cherry = 1
+plum = 2
+bells = 3
+clover = 4
+lemon = 5
+coin = 6
+seven = 7
+bar = 8
+apple = 9
+heart = 10
+grape = 11
+diamond = 12
+orange = 13
+horse_shoe = 14
+watermelon = 15
+
+print("Enter a coin to start")
+player = int(input("Press 1 to start"))
 
 
-print(" Hello world")
 
 
 
 
-print("give a number")
-x = input("")
 
-print("here is a random number")
-if random.randint(1,1000) + int(x):
-    print(random.randint(1,1000))
-else:
-    print("now what is the anwser")
+
+
+
+
+
+
+
+
+
+
+
+def slot_machine():
+    print("Enter a coin to start ")
+    player = random.randint(1 , 15),random.randint(1 , 15),random.randint(1 , 15)
+    print(player)
+
+
+
+
 
 
 
