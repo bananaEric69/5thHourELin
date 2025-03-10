8151b25de1e9a51812a7136b01beaa1f4834deaa
Eric Lin <el128221@Student.wpsok.org>
2025-03-10 13:19:08 -0500

    Added HW23 and Scenario 8

---
+++ HW23.py
@@ -0,0 +1,50 @@
+#Name:Eric Lin
+#Class: 5th Hour
+#Assignment: HW23
+
+#1. Import the random and time libraries
+import random
+import time
+
+
+
+#2. Create a class containing a def function that inits self and the 3 attributes health, damage, and speed.
+class Stat:
+    def __init__(self, health, damage, speed):
+        self.health = health
+        self.damage = damage
+        self.speed = speed
+
+    def attack(self):
+        for a in range(0, 11, + 1):
+            time.sleep(1)
+            print(a)
+
+    attack()
+
+
+
+#3. Make a "warrior" character object with 100 health, 20 damage, and 30 speed. Print the character's initial health below.
+Warrior = Stat(100, 20, 30)
+print(f"Warrior health is at {Warrior.health}")
+#4. Make a def function within the class that loops 10 times. Within this function,
+#make the following loop 10 times: the character takes a random amount of damage from 1 to 6,
+#the new health is printed, a time.sleep delay of one second is done. Call the function to the warrior.
+
+
+
+
+
+#5. Make a "healer" character object with 60 health, 10 damage, and 30 speed.
+Healer = Stat(60, 10, 30)
+#6. Make a def function within the class that heals the warrior for 30 health. Create an if statement
+#that sets the warrior's health to its max (100) if the healing would bring the warrior's health above that.
+#Call the function to the healer.
+
+#7. Print the warrior's final health at the very bottom.
+
+
+
+
+
+
