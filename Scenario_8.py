322a198d3c2bb3ae69bc9e1f2701160b1d0ea15c
Eric Lin <el128221@Student.wpsok.org>
2025-03-11 13:18:08 -0500

    Changes to Scenario 8

--- Scenario_8.py
+++ Scenario_8.py
@@ -6,6 +6,22 @@ import random
 #Scenario 8:
 #With a fresh perspective, the team lead wants you to look back and refactor the old combat code to
 #be streamlined with classes so the character and enemy stats won't be built in bulky dictionaries anymore.
+class Game:
+    def __init__(self,health,ac,damage,attack_mod):
+        self.health = health
+        self.ac = ac
+        self.damage = damage
+        self.attack_mod = attack_mod
+
+LaeZel = Game(12,17,random.randint(1,6) +  random.randint(1,6) + 3,4 )
+ShadowHeart = Game(10,14,random.randint(1,6) + 2,4 )
+Gale = Game(8,14,random.randint(1,6),4 )
+Astarion = Game(10,14,random.randint(1,6) + 4,4 )
+
+Pirate_Monkey = Game(20,14,random.randint(1,6) +  random.randint(1,6) + 1,4 )
+Farmer = Game(10, 10,random.randint(1,6),4 )
+Wizard = Game(10,10,random.randint(1,6),4 )
+Knight = Game(15,10,random.randint(1,6) + 2,4 )
 
 #(Translation: Rebuild Scenario 3 using classes instead of dictionaries, include the combat test code
 #below as well.)
